739b88950879062de89417d16bf80ba7699a8262
choy2007 <choymanzanoalberto@gmail.com>
2023-04-23 01:22:47 +0800

    Add book scaffold

---
+++ db/migrate/20230422172211_create_books.rb
@@ -0,0 +1,15 @@
+class CreateBooks < ActiveRecord::Migration[6.1]
+  def change
+    create_table :books do |t|
+      t.string :title
+      t.string :isbn
+      t.decimal :price
+      t.string :publication_year
+      t.references :publisher, null: false, foreign_key: true
+      t.string :image_url
+      t.string :edition
+
+      t.timestamps
+    end
+  end
+end
