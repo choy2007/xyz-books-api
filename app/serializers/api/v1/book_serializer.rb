edd0ceca6d95d97678ae56038e496ddd4f5d1086
choy2007 <choymanzanoalberto@gmail.com>
2023-04-24 03:33:49 +0800

    Relocate book serilizer to api v1

--- app/serializers/api/v1/book_serializer.rb
+++ app/serializers/api/v1/book_serializer.rb
@@ -1,10 +1,11 @@
-class BookSerializer
+class Api::V1::BookSerializer
   include JSONAPI::Serializer
 
+  set_id :isbn
   attributes :title, :isbn, :publication_year, :edition, :price
 
   attribute :author do |object|
-    "#{object.first.authors.pluck(:first_name, :middle_name, :last_name).map { |a| a.compact.join(' ') }.join(', ')}"
+    "#{object.authors.pluck(:first_name, :middle_name, :last_name).map { |a| a.compact.join(' ') }.join(', ')}"
   end
 
   attribute :publisher do |object|
