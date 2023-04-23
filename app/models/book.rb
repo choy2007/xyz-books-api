0627c73976f7779b6c9276a5da5937a8d5c4e0da
choy2007 <choymanzanoalberto@gmail.com>
2023-04-23 21:32:33 +0800

    Add has many relation to book

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,8 @@
 class Book < ApplicationRecord
   belongs_to :publisher
 
+  has_many :book_authors
+  has_many :authors, through: :book_authors
+
   validates_presence_of :title, :isbn, :price, :publication_year, :publisher
 end
